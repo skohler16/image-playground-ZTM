04e8b9bfcb2c8dc07dffc5403f7a36f7fdb9f00d
Scooter C Kohler <skohler16@gmail.com>
2024-08-06 14:15:19 -0700

    created a JPG to PNG converter

--- JPGtoPNGconvert.py
+++ JPGtoPNGconvert.py
@@ -3,3 +3,17 @@ import os
 from PIL import Image
 
 
+JPG_folder = sys.argv[1]
+PNG_folder = sys.argv[2]
+
+if not os.path.exists(PNG_folder):
+    os.makedirs(PNG_folder)
+
+for filename in os.listdir(JPG_folder):
+    try:
+        img = Image.open(f'{JPG_folder}{filename}')
+        clean_name = os.path.splitext(filename)[0]
+        img.save(f'{PNG_folder}{clean_name}.png','png')
+        print('all done')
+    except OSError:
+        print('cannot convert those files')
